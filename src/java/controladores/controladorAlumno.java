7fde7f4c5142522db3bb37e2c275ca2547ca83a9
diego10j.89 <diego10j.89@gmail.com>
2013-12-11 21:44:55 +0000

    enlazar pagina con controlador y servicio

--- src/java/controladores/controladorAlumno.java
+++ src/java/controladores/controladorAlumno.java
@@ -4,6 +4,7 @@
  */
 package controladores;
 
+import aplicacion.Utilitario;
 import entidades.Alumnos;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -20,9 +21,28 @@ public class controladorAlumno {
 
     @EJB
     private servicioAlumno servAlumno;
+    private Alumnos aluAlumno = new Alumnos();
+    private Utilitario utilitario = new Utilitario();
 
     public void guardar() {
-        System.out.println("ENTRO AL GUARDAR");
+        if (utilitario.validarCedula(aluAlumno.getAluCedula())) {
+            String str_mensaje = servAlumno.guardarAluno(aluAlumno);
+            if (str_mensaje.isEmpty()) {
+                utilitario.agregarMensaje("Se guardo correctamente", "");
+                aluAlumno = new Alumnos();
+            } else {
+                utilitario.agregarMensajeError("No se pudo guardar", str_mensaje);
+            }
+        } else {
+            utilitario.agregarMensajeInfo("La cédula ingresada no es válida", "");
+        }
+    }
 
+    public Alumnos getAluAlumno() {
+        return aluAlumno;
+    }
+
+    public void setAluAlumno(Alumnos aluAlumno) {
+        this.aluAlumno = aluAlumno;
     }
 }
