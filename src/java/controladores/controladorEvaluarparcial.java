058c946450eb7b5dcb3e5c5866a14a511efc2fb7
diego10j.89 <diego10j.89@gmail.com>
2014-01-29 21:07:11 +0000

    Avances

---
+++ src/java/controladores/controladorEvaluarparcial.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import aplicacion.Utilitario;
+import entidades.Evaluarparcial;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import servcios.servicioEvaluarparcial;
+import servcios.servicioFormaevaluar;
+
+/**
+ *
+ * @author Diego
+ */
+@ManagedBean
+@ViewScoped
+public class controladorEvaluarparcial {
+
+    @EJB
+    private servicioEvaluarparcial servEvaluarparcial;
+    private Evaluarparcial evaParcial = new Evaluarparcial();
+    private Utilitario utilitario = new Utilitario();
+    private List<Evaluarparcial> listaEvaluarparcial;
+    private List<Evaluarparcial> filtroEvaluarparcial;
+
+    @PostConstruct
+    public void cargarDatos() {
+        listaEvaluarparcial = servEvaluarparcial.getEvaluarparcials();
+    }
+    
+    public void insertar(){
+        evaParcial = new Evaluarparcial();
+    }
+    
+     public void eliminar() {
+        if (evaParcial.getEvaCodigo() != null) {
+            String str_mensaje = servEvaluarparcial.elimnarEvaluarparcial(evaParcial.getEvaCodigo().toString());
+            if (str_mensaje.isEmpty()) {
+                utilitario.agregarMensaje("Se elimino correctamente", "");
+                cargarDatos();
+            } else {
+                utilitario.agregarMensajeError("No se puede eliminar " + str_mensaje, "");
+            }
+        }
+    }
+
+    public void guardar() {
+        
+            String str_mensaje = servEvaluarparcial.guardarEvaluarparcial(evaParcial);
+            if (str_mensaje.isEmpty()) {
+                utilitario.agregarMensaje("Se guardo correctamente", "");
+                evaParcial = new Evaluarparcial();
+                cargarDatos();
+                utilitario.ejecutarJavaScript("wdlgDetalle.hide()");
+            } else {
+                utilitario.agregarMensajeError("No se pudo guardar", str_mensaje);
+            }
+       
+    }
+
+    public Evaluarparcial getEvaParcial() {
+        return evaParcial;
+    }
+
+    public void setEvaParcial(Evaluarparcial evaParcial) {
+        this.evaParcial = evaParcial;
+    }
+
+    public List<Evaluarparcial> getListaEvaluarparcial() {
+        return listaEvaluarparcial;
+    }
+
+    public void setListaEvaluarparcial(List<Evaluarparcial> listaEvaluarparcial) {
+        this.listaEvaluarparcial = listaEvaluarparcial;
+    }
+
+    public List<Evaluarparcial> getFiltroEvaluarparcial() {
+        return filtroEvaluarparcial;
+    }
+
+    public void setFiltroEvaluarparcial(List<Evaluarparcial> filtroEvaluarparcial) {
+        this.filtroEvaluarparcial = filtroEvaluarparcial;
+    }
+    
+  
+}
---
+++ src/java/servcios/servicioEvaluarparcial.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servcios;
+
+import aplicacion.Utilitario;
+import entidades.Evaluarparcial;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author Diego
+ */
+@Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
+public class servicioEvaluarparcial {
+
+    @PersistenceContext
+    private EntityManager manejador;
+    @Resource
+    private UserTransaction utx;
+    private Utilitario utilitario = new Utilitario();
+
+    public String guardarEvaluarparcial(Evaluarparcial evaluarparcial) {
+        try {
+            utx.begin();
+            manejador.joinTransaction();
+            if (evaluarparcial.getEvaCodigo() == null) {
+                //nombre tabla y atributo
+                long lon_codigo = utilitario.getConexion().getMaximo("evaluarparcial", "eva_codigo", 1);
+                System.out.println("ide " + lon_codigo);
+                evaluarparcial.setEvaCodigo(new Integer(String.valueOf(lon_codigo)));
+                manejador.persist(evaluarparcial);
+            } else {
+                manejador.merge(evaluarparcial);
+            }
+
+
+            utx.commit();
+        } catch (Exception e) {
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+            }
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        return "";
+    }
+
+    public String elimnarEvaluarparcial(String evaCodigo) {
+        try {
+            utx.begin();
+            Evaluarparcial borraEvaluarparcial = manejador.find(Evaluarparcial.class, new Integer(evaCodigo));
+            manejador.remove(borraEvaluarparcial);
+            utx.commit();
+        } catch (Exception e) {
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+            }
+            return e.getMessage();
+        }
+        return "";
+    }
+
+    public Evaluarparcial getEvaluarparcial() {
+
+        try {
+            Query q = manejador.createNamedQuery("Evaluarparcial.findByEvaCodigo");
+            q.setParameter("evaCodigo", new Integer("0"));
+            return (Evaluarparcial) q.getSingleResult();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public List<Evaluarparcial> getEvaluarparcials() {
+        try {
+            Query q = manejador.createNamedQuery("Evaluarparcial.findAll");
+            return q.getResultList();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+}
